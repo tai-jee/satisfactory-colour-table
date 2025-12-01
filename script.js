4914585c5943f74c5fc58f6566ba91d62638ad68
tai-jee <itsme@taijee.xyz>
2025-12-01 11:52:54 +0000

    update colours json file to a json module for better organisation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { colours } from "./colours/colours.js";
+
 const ficsitTickURL =
 	"https://satisfactory.wiki.gg/images/1/1d/FICSIT_Inc..png";
 
@@ -35,73 +37,69 @@ sfLogos.forEach((sfLogo) => {
 });
 
 function loadResults() {
-	fetch("colours/colours.json")
-		.then((response) => response.json())
-		.then((data) => {
-			const results = document.getElementById("results");
+	const results = document.getElementById("results");
 
-			results.innerHTML = "";
+	results.innerHTML = "";
 
-			const colours = data.colours.sort((a, b) =>
-				a.aliases[0].localeCompare(b.aliases[0])
+	const colourNames = colours.sort((a, b) =>
+		a.aliases[0].localeCompare(b.aliases[0])
+	);
+
+	let selectedCategories = [];
+	checkboxes.forEach((checkbox) => {
+		if (
+			checkbox
+				.getElementsByClassName("checkbox")[0]
+				.classList.contains("checked")
+		) {
+			selectedCategories.push(checkbox.querySelector("label").innerText);
+		}
+	});
+
+	// add colours if any of their aliases match the contents of the search bar
+	// and their category matches the selected checkbox(es)
+	colours.forEach((colour) => {
+		if (
+			colour.aliases.some((alias) => {
+				return searchInput.value === ""
+					? true
+					: alias.toLowerCase().includes(searchInput.value.toLowerCase());
+			}) &&
+			// add colours if at least one of their categories matches the selected checkbox(es)
+			// or if no checkboxes are selected
+			colour.categories.some((category) => {
+				return selectedCategories.length === 0
+					? true
+					: selectedCategories.includes(category);
+			})
+		) {
+			const colourItem = document.createElement("div");
+			colourItem.classList.add("colour-item");
+
+			const colourChip = colourItem.appendChild(
+				document.createElement("div")
 			);
+			colourChip.classList.add("colour-cell");
+			colourChip.style.backgroundColor = "#" + colour.colour;
 
-			let selectedCategories = [];
-			checkboxes.forEach((checkbox) => {
-				if (
-					checkbox
-						.getElementsByClassName("checkbox")[0]
-						.classList.contains("checked")
-				) {
-					selectedCategories.push(checkbox.querySelector("label").innerText);
-				}
-			});
+			const colourName = colourItem.appendChild(
+				document.createElement("div")
+			);
+			colourName.classList.add("name-cell");
+			colourName.innerHTML = `<span class="colour-name">${colour.aliases[0]}</span><span class="colour-code">#${colour.colour}</span>`;
 
-			// add colours if any of their aliases match the contents of the search bar
-			// and their category matches the selected checkbox(es)
-			colours.forEach((colour) => {
-				if (
-					colour.aliases.some((alias) => {
-						return searchInput.value === ""
-							? true
-							: alias.toLowerCase().includes(searchInput.value.toLowerCase());
-					}) &&
-					// add colours if at least one of their categories matches the selected checkbox(es)
-					// or if no checkboxes are selected
-					colour.categories.some((category) => {
-						return selectedCategories.length === 0
-							? true
-							: selectedCategories.includes(category);
-					})
-				) {
-					const colourItem = document.createElement("div");
-					colourItem.classList.add("colour-item");
+			results.appendChild(colourItem);
 
-					const colourChip = colourItem.appendChild(
-						document.createElement("div")
-					);
-					colourChip.classList.add("colour-cell");
-					colourChip.style.backgroundColor = "#" + colour.colour;
-
-					const colourName = colourItem.appendChild(
-						document.createElement("div")
-					);
-					colourName.classList.add("name-cell");
+			colourItem.addEventListener("click", () => {
+				navigator.clipboard.writeText(`#${colour.colour}`);
+				colourName.innerText = "Copied!";
+				setTimeout(() => {
 					colourName.innerHTML = `<span class="colour-name">${colour.aliases[0]}</span><span class="colour-code">#${colour.colour}</span>`;
-
-					results.appendChild(colourItem);
-
-					colourItem.addEventListener("click", () => {
-						navigator.clipboard.writeText(`#${colour.colour}`);
-						colourName.innerText = "Copied!";
-						setTimeout(() => {
-							colourName.innerHTML = `<span class="colour-name">${colour.aliases[0]}</span><span class="colour-code">#${colour.colour}</span>`;
-						}, 1000);
-					});
-				}
+				}, 1000);
 			});
-		});
-}
+		}
+	});
+};
 
 loadResults();
 
